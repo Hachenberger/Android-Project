f2b3952fcf20de667b25fc2080dcb02930d94cf8
Hachenberger <kay.hachenberger@rub.de>
2016-07-19 14:24:15 +0200

    Created Activities
    Get, Post und Archive Activities created

---
+++ app/src/main/java/praktikum/androidproject/ArchiveActivity.java
@@ -0,0 +1,17 @@
+package praktikum.androidproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Kay on 19.07.2016.
+ */
+public class ArchiveActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_archive);
+    }
+}
--- app/src/main/java/praktikum/androidproject/MainActivity.java
+++ app/src/main/java/praktikum/androidproject/MainActivity.java
@@ -1,7 +1,11 @@
 package praktikum.androidproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_archive = (Button) findViewById(R.id.button_start_archive_activity);
+        Button btn_get = (Button) findViewById(R.id.button_start_get_activity);
+        Button btn_post = (Button) findViewById(R.id.button_start_post_activity);
+
+
+        // ClickListener implementieren für den Button zum Wechsel der Activity
+        btn_archive.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                //Neues Intent anlegen
+                Intent archive_intent = new Intent(getApplicationContext(), ArchiveActivity.class);
+
+                // Intent starten und zur zweiten Activity wechseln
+                startActivity(archive_intent);
+
+            }
+        });
+
+        btn_get.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                //Neues Intent anlegen
+                Intent get_intent = new Intent(getApplicationContext(), GetActivity.class);
+
+                // Intent starten und zur zweiten Activity wechseln
+                startActivity(get_intent);
+
+            }
+        });
+
+        btn_post.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                //Neues Intent anlegen
+                Intent post_intent = new Intent(getApplicationContext(), PostActivity.class);
+
+                // Intent starten und zur zweiten Activity wechseln
+                startActivity(post_intent);
+
+            }
+        });
+
     }
 }
